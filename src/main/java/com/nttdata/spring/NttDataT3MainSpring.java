2657c3b2a77e5aa5d05999857542852ce2596d0d
Ernesto <ernestocf2418@gmail.com>
2023-01-09 00:17:17 +0100

    Segundo Commit

--- src/main/java/com/nttdata/spring/NttDataT3MainSpring.java
+++ src/main/java/com/nttdata/spring/NttDataT3MainSpring.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 import com.nttdata.spring.repository.Customers;
 import com.nttdata.spring.services.CustomersManagementServiceI;
